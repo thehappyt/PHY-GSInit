55803b925fe636cf973ed57300506d9e2ba6eee3
thehappyt <thompson.adams@gmail.com>
2014-08-16 02:40:39 -0700

    Update gsapp.js

--- gsapp.js
+++ gsapp.js
@@ -41,6 +41,19 @@
     }
     GSapp.prototype.constructor = GSapp
 
+    Object.defineProperty(canvas.prototype, "out",     {configurable: false, enumerable: true,  writable: true,
+        value: function()  { return this.forward.multiply(-1).norm();            } })
+    Object.defineProperty(canvas.prototype, "right",   {configurable: false, enumerable: true,  writable: true,
+        value: function()  { return norm(this.up.cross(this.out())); } })
+    Object.defineProperty(canvas.prototype, "top",     {configurable: false, enumerable: true,  writable: true,
+        value: function()  { return this.out().cross(this.right());  } })
+    Object.defineProperty(canvas.prototype, "inPlane", {configurable: false, enumerable: true,  writable: true,
+        value: function(pos) {
+            if (!(pos instanceof vec)) throw new Error ("Argument 'pos' for inPlane(pos) function must be a vector.");
+            return pos.sub((pos.sub(this.center)).proj(this.out()));
+        }
+    })
+
     var global = window
     function Export( exports ) {
         if (!global.gsapp) global.gsapp = {}
