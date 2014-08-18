267c8b390b3e8b70315ea0b41204bde0f895918f
thehappyt <thompson.adams@gmail.com>
2014-08-18 13:55:42 -0700

    Update gsapp.js

--- gsapp.js
+++ gsapp.js
@@ -29,6 +29,7 @@
         
         this.menuitem = function(mi) { if (mi instanceof jQuery && (mi.is("li") || mi.is("ul"))) this.canvas.menubar.append(mi.gsmenubar()); }
         
+        // Syntax: (GSAppobj).iconitem({title, iconclasses, text, checked}, cb)
         this.iconitem = function(opt,cb) {
             var b, text=opt.text||false;
             b = $('<button' opt.title?(' title="' + opt.title + '"'):('>') + text?(opt.text+''):('')+'</button>')
@@ -38,7 +39,6 @@
             this.canvas.iconbar.append(b);
             if (cb && (Object.prototype.toString.call(cb) == '[object Function]')) cb();
         }
-iconitem({title, iconclasses, text, checked}, cb)
 
         var myStylesLocation = "https://raw.githack.com/thehappyt/PHY-GSInit/master/gsapp.css";
         $('<link rel="stylesheet" type="text/css" href="'+myStylesLocation+'" >') .appendTo("head");
