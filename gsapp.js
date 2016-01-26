6ba0a3ba29ffb86e2107e955c7cbca8f7a678797
thehappyt <thompson.adams@gmail.com>
2016-01-25 23:33:26 -0700

    Optional MenuBar and IconBar
    Added Boolean parameters as options to turn on the Menubar and Iconbar.
    By default, the Menubar and Iconbar are turned off.

--- gsapp.js
+++ gsapp.js
@@ -4,18 +4,23 @@
     function GSapp(options) {
         if (!(this instanceof GSapp)) return new GSapp(options);
         options = options || {}
+        if (!options.title) options.title = '';
+        if (!options.caption) options.caption = '';
+        if (!options.mbar) options.mbar = false;
+        if (!options.ibar) options.ibar = false;
+
         this.canvas = canvas({ resizable:false })
-        this.appst = function(ttl, cptn) {       // SETUP APPLICATION STRUCTURE
-            var high = Number(window.innerHeight) - 78              // 36 for status bar, 21 for title, 24 for menubar, 21 for iconbar
-            var wide = Number(window.innerWidth) - 20               // 20 for good measure
+        this.appst = function(ttl, cptn, mbar, ibar) {                                              // SETUP APPLICATION STRUCTURE
+            var high = Number(window.innerHeight) - 57 - 24*(mbar||false) - 21*(ibar||false)        // 36 for status bar, 21 for title
+            var wide = Number(window.innerWidth) - 20                                               // 20 for good measure
 
             this.canvas.width=high
             this.canvas.height=high
             if (!this.canvas.__activated) sphere({ canvas: this.canvas, visible: false })
             this.canvas.title.text(ttl);
             this.canvas.caption.text(cptn);
-            this.canvas.menubar = $("<div/>").css("white-space","pre").insertBefore(this.canvas.wrapper)
-            this.canvas.iconbar = $("<div/>").css("white-space","pre").insertBefore(this.canvas.wrapper)
+            if (mbar): this.canvas.menubar = $("<div/>").css("white-space","pre").insertBefore(this.canvas.wrapper);
+            if (ibar): this.canvas.iconbar = $("<div/>").css("white-space","pre").insertBefore(this.canvas.wrapper);
             this.canvas.wrapper.css({margin: '4px'})
             $(this.canvas.__canvas_element).css({border: '1px solid #AAA'})
             $(this.canvas.__overlay_element).css({border: '1px solid #AAA'})
@@ -27,10 +32,12 @@
             this.canvas.iconbar.addClass("gsiconbar")
         }
         
-        this.menuitem = function(mi) { if (mi instanceof jQuery && (mi.is("li") || mi.is("ul"))) this.canvas.menubar.append(mi.gsmenubar()); }
-        
-        // Syntax: (GSAppobj).iconitem({title, iconclasses, text}, cb)
-        this.iconitem = function(opt) {
+        if (options.mbar) {
+            this.menuitem = function(mi) { if (mi instanceof jQuery && (mi.is("li") || mi.is("ul"))) this.canvas.menubar.append(mi.gsmenubar()); }
+        }
+        if (options.ibar) {
+            // Syntax: (GSAppobj).iconitem({title, iconclasses, text}, cb)
+            this.iconitem = function(opt) {
             var b = $('<button>'), text=opt.text||false;
             if (opt.title) { opt.title = opt.title + ''; b.attr({ title: opt.title }); }
             //if (opt.text) { opt.text = opt.text + ''; b.text( opt.text ); } //(NOT WORKING YET  b.css("width", "50px"))
@@ -39,15 +46,16 @@
             this.canvas.iconbar.append(b);
             return b;
         }
+        }
 
         var myStylesLocation = "https://raw.githack.com/thehappyt/PHY-GSInit/master/gsapp.css";
         $('<link rel="stylesheet" type="text/css" href="'+myStylesLocation+'" >') .appendTo("head");
 
-        if (!options.title) options.title = '';
-        if (!options.caption) options.caption = '';
-        this.appst(options.title, options.caption);
+        this.appst(options.title, options.caption, options.mbar, options.ibar);
         delete options.title;
         delete options.caption;
+        delete options.mbar;
+        delete options.ibar;
         for (var id in options) this.canvas[id] = options[id];
         
     }
