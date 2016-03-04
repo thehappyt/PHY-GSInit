0318952282aa0b4426bda98dc477e3c2e29e3257
Adam Thompson <thompson.adams@gmail.com>
2016-03-03 23:45:15 -0700

    Attempt to allow non-fill circle.

--- gsobj.js
+++ gsobj.js
@@ -37,6 +37,7 @@
         }
         
         var R = args.radius || 1; delete args.radius;
+        args.fill = args.fill || false;
         args.pos = args.pos || vec(0,0,0);
         args.axis = args.axis || vec(1,0,0);
         args.up = args.up || vec(0,1,0);
@@ -47,21 +48,31 @@
         args.emissive = args.emissive || 0;
         
         var m = makeCircle(args);
-        var v = [];
-        for (var i=0; i<m.pos.length; i+=1) {
-            var mi = m.pos[i];
-            v.push( vertex({    pos: vec(R*mi.x, R*mi.y, R*mi.z),   normal: vec(1,0,0),
-                                color: args.color,                  opacity: args.opacity,
-                                shininess: args.shininess,          emissive: args.emissive
-            }) );
+
+        if (args.fill) {
+            var v = [];
+            for (var i=0; i<m.pos.length; i++) {
+                var mi = m.pos[i];
+                v.push( vertex({    pos: vec(R*mi.x, R*mi.y, R*mi.z),   normal: vec(1,0,0),
+                                    color: args.color,                  opacity: args.opacity,
+                                    shininess: args.shininess,          emissive: args.emissive
+                }) );
+            }
+            
+            var t = [];
+            for (var i=0; i<m.index.length; i+=3) {
+                t.push( triangle({ v0: v[m.index[i]], v1: v[m.index[i+1]], v2: v[m.index[i+2]] }) );
+            }
+            
+            return compound( t, { pos: args.pos, axis: args.axis, up: args.up } );
+        } else {
+            var p = m.pos.slice(1);
+            for (var i=0; i<p.length; i++) { p[i] = vec(R*p[i].x,R*p[i].y,R*p[i].z)}
+            p.push(p[0]);
+            return curve({radius: 0.02*R, origin: args.pos, pos=p, axis=args.axis, up: args.up,
+                        color: args.color, opacity: args.opacity, shininess: args.shininess, 
+                        emissive: args.emissive })
         }
-        
-        var t = [];
-        for (var i=0; i<m.index.length; i+=3) {
-            t.push( triangle({ v0: v[m.index[i]], v1: v[m.index[i+1]], v2: v[m.index[i+2]] }) );
-        }
-        
-        return compound( t, { pos: args.pos, axis: args.axis, up: args.up } );
     }
     
     var global = window
