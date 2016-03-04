ca93a2367cc5d90a406560128a6c708d049a4748
Adam Thompson <thompson.adams@gmail.com>
2016-03-04 00:03:19 -0700

    Error with default value for fill.

--- gsobj.js
+++ gsobj.js
@@ -42,7 +42,7 @@
         }
         
         var R = args.radius || 1; delete args.radius;
-        args.fill = args.fill || false;
+        args.fill = args.fill || true;
         args.pos = args.pos || vec(0,0,0);
         args.axis = args.axis || vec(1,0,0);
         args.up = args.up || vec(0,1,0);
